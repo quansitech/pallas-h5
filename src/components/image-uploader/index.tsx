ab353aecc73091d654a2e0432a351a07e847677f
tider <tider@quansitech.com>
2024-01-19 14:30:00 +0800

    feat: 修复打包时加载.d.ts文件报错

--- src/components/image-uploader/index.tsx
+++ src/components/image-uploader/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { ImageUploader as AntdImageUploader } from "antd-mobile";
-import init, {calc_file_hash} from '@quansitech/file-md5-wasm';
-import type { ImageUploadItem } from "antd-mobile";
-import "./index.less";
+import init, { calc_file_hash } from '@quansitech/file-md5-wasm';
+import type { ImageUploadItem } from 'antd-mobile';
+import { ImageUploader as AntdImageUploader } from 'antd-mobile';
+import React from 'react';
+import './index.less';
+import cosStorage from './storage/cos';
+import ossStorage from './storage/oss';
+import serverStorage from './storage/server';
+import tosStorage from './storage/tos';
 
 interface ImageUploaderProps {
   className?: string;
@@ -14,55 +18,65 @@ interface ImageUploaderProps {
 }
 
 interface StorageProps {
-  file: File,
-  action: string,
-  hashId?: string
+  file: File;
+  action: string;
+  hashId?: string;
 }
 
-const factoryStorage = async (uploadTo: string) => {
-  const storage =  await import(`./storage/${uploadTo}`);
-  return storage.default;
-}
-
+const factoryStorage = (uploadTo: string) => {
+  if (uploadTo === 'cos') {
+    return cosStorage;
+  }
+  if (uploadTo === 'oss') {
+    return ossStorage;
+  }
+  if (uploadTo === 'tos') {
+    return tosStorage;
+  }
+  if (uploadTo === 'server') {
+    return serverStorage;
+  }
+};
 
 export const ImageUploader: React.FC<ImageUploaderProps> = ({
-    className="pallas-image-uploader", 
-    value = [], 
-    hashCheck=true, 
-    uploadTo="server",
-    action, 
-    onChange}) => {
-
-    const upload = async (file: File) => {
-        const params: StorageProps = {
-          file,
-          action,
-          hashId: ''
-        };
-        if(hashCheck){
-          await init();
-          params.hashId = await calc_file_hash(file);
-        }
-
-        const storage = await factoryStorage(uploadTo);
-
-        const res = await storage.upload(params.file, params.action, params.hashId);
-        
-        return res;
-        
+  className = 'pallas-image-uploader',
+  value = [],
+  hashCheck = true,
+  uploadTo = 'server',
+  action,
+  onChange,
+}) => {
+  const upload = async (file: File) => {
+    const params: StorageProps = {
+      file,
+      action,
+      hashId: '',
+    };
+    if (hashCheck) {
+      await init();
+      params.hashId = await calc_file_hash(file);
     }
 
-    const handleChange = (items: ImageUploadItem[]) => {
-      onChange && onChange(items);
+    const storage = factoryStorage(uploadTo);
+
+    const res = storage?.upload(params.file, params.action, params.hashId);
+
+    return res;
+  };
+
+  const handleChange = (items: ImageUploadItem[]) => {
+    if (typeof onChange === 'function') {
+      onChange(items);
     }
+  };
 
-
-    return <AntdImageUploader className={className}
-          value={value}
-          onChange={handleChange} 
-          upload={upload}
-          multiple={true}
+  return (
+    <AntdImageUploader
+      className={className}
+      value={value}
+      onChange={handleChange}
+      upload={upload}
+      multiple={true}
     />
-    
-}
-
+  );
+};
--- src/components/image-uploader/storage/cos.ts
+++ src/components/image-uploader/storage/cos.ts
@@ -1,51 +1,54 @@
+import Utils from '../utils';
+import type { Storage } from './storage-type';
 
-import Utils from "../utils";
+const cosStorage: Storage = {
+  upload: async (file: File, action: string, hashId?: string) => {
+    const formData = new FormData();
 
-export default {
-    upload: async (file: File, action: string, hashId?: string) => {
-
-        const formData = new FormData();
-
-        let preUrl = new URLSearchParams(action);
-        hashId && preUrl.append("hash_id", hashId);
-
-        let fileType = file.type;
-        if(!fileType){
-            fileType = await Utils.getFileType.start(file);
-        }
-        preUrl.append('vendor_type', "tengxun_cos");
-        preUrl.append('file_type', fileType);
-        preUrl.append('title', file.name);
-
-        const res = await fetch(decodeURIComponent(preUrl.toString()));
-        const resData = await res.json();
-        if(parseInt(resData.status) === 2){
-            return {
-                file_id: resData.file_id,
-                url: resData.file_url
-            };
-        }
-
-        
-        const new_multipart_params = {
-            "Content-type": fileType,
-            ...resData.params
-        }
-
-        for(let key in new_multipart_params){
-            formData.append(key, new_multipart_params[key]);
-        }
-
-        formData.append("file", file);
-
-        const response = await fetch(resData['url'], {
-            method: 'POST',
-            body: formData
-        });
-
-        const data = await response.json();
-        
-        return data;
-
+    let preUrl = new URLSearchParams(action);
+    if (typeof hashId !== 'undefined' && hashId !== '') {
+      preUrl.append('hash_id', hashId);
     }
-}
+
+    let fileType = file.type;
+    if (!fileType) {
+      fileType = await Utils.getFileType.start(file);
+    }
+    preUrl.append('vendor_type', 'tengxun_cos');
+    preUrl.append('file_type', fileType);
+    preUrl.append('title', file.name);
+
+    const res = await fetch(decodeURIComponent(preUrl.toString()));
+    const resData = await res.json();
+    if (parseInt(resData.status) === 2) {
+      return {
+        file_id: resData.file_id,
+        url: resData.file_url,
+      };
+    }
+
+    const new_multipart_params = {
+      'Content-type': fileType,
+      ...resData.params,
+    };
+
+    for (let key in new_multipart_params) {
+      if (new_multipart_params.hasOwnProperty(key)) {
+        formData.append(key, new_multipart_params[key]);
+      }
+    }
+
+    formData.append('file', file);
+
+    const response = await fetch(resData['url'], {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    return data;
+  },
+};
+
+export default cosStorage;
